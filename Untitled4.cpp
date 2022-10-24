cd91638f67cd1ebf27de19e80a675ed280dd6bf5
emrkrkss <99718205+emrkrkss@users.noreply.github.com>
2022-10-24 15:59:30 +0300

    pointer 4

---
+++ Untitled4.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void){
+	int x=10;
+	int *p = &x;
+	printf("Xin degeri = %d\n", x);
+	printf("Xin adresi = %p\n", &x);
+	printf("Xin adresi = %d\n", p);
+	printf("Xin degeri = %d\n", *p);
+	
+	
+	
+	
+	
+}
